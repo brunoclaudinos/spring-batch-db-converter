5e672ad37beec3ecfce9f9df9791d8acf0d1af29
Bruno <brunosantos-bn@live.com>
2022-01-26 21:41:25 -0300

    Project created

---
+++ src/main/java/com/brunoclaudinos/dbconverter/DbConverterApplication.java
@@ -0,0 +1,13 @@
+package com.brunoclaudinos.dbconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbConverterApplication.class, args);
+	}
+
+}
